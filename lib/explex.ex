176a123eea191b2e9fc59c8c6053a67d5d5f342d
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2013-12-27 21:38:22 +0100

    Add resolution algorithm

--- lib/explex.ex
+++ lib/explex.ex
@@ -1,2 +1,116 @@
 defmodule Explex do
+  defrecord Package, [:name, :version, :deps]
+
+  defrecord State, [:activated, :pending]
+  defrecord Request, [:name, :req, :parent]
+  defrecord Active, [:name, :version, :state, :parents, :possibles]
+
+  @ets_table :explex_registry
+
+  def start do
+    opts = [
+      :bag,
+      { :keypos, Package.__record__(:index, :name) + 1 },
+      :named_table,
+      :public ]
+    :ets.new(@ets_table, opts)
+  end
+
+  def stop do
+    :ets.delete(@ets_table)
+  end
+
+  def add_packages(packages) do
+    :ets.insert(@ets_table, packages)
+  end
+
+  def resolve(pending) do
+    requests = Enum.map(pending, fn { name, req } ->
+      Request[name: name, req: req]
+    end)
+    resolve(HashDict.new, requests)
+  end
+
+  defp resolve(activated, []) do
+    Enum.map(activated, fn { name, Active[] = active } ->
+      { name, active.version }
+    end) |> Enum.reverse
+  end
+
+  defp resolve(activated, [request|pending]) do
+    active = activated[request.name]
+
+    if active do
+      possibles = Enum.filter(active.possibles, &version_match?(&1, request.req))
+      active = active.possibles(possibles).parents(wrap(request.parent) ++ active.parents)
+
+      if Version.match?(active.version, request.req) do
+        activated = Dict.put(activated, request.name, active)
+        resolve(activated, pending)
+      else
+        backtrack(active, activated)
+      end
+    else
+      versions = get_versions(request.name)
+        |> Enum.filter(&version_match?(&1, request.req))
+
+      case versions do
+        [] ->
+          backtrack(activated[request.parent], activated)
+
+        [version|possibles] ->
+          new_pending = get_deps(request.name, version)
+          state = State[activated: activated, pending: pending]
+          new_active = Active[name: request.name, version: version, state: state,
+                              possibles: possibles, parents: wrap(request.parent)]
+          activated = Dict.put(activated, request.name, new_active)
+
+          resolve(activated, pending ++ new_pending)
+      end
+    end
+  end
+
+  defp backtrack(nil, _activated) do
+    nil
+  end
+
+  defp backtrack(Active[state: state] = active, activated) do
+    case active.possibles do
+      [] ->
+        Enum.find_value(active.parents, fn parent ->
+          backtrack(activated[parent], activated)
+        end)
+
+      [version|possibles] ->
+        active = active.possibles(possibles).version(version)
+        pending = get_deps(active.name, version)
+
+        activated = Dict.put(state.activated, active.name, active)
+        resolve(activated, state.pending ++ pending)
+    end
+  end
+
+  defp version_match?(_version, nil), do: true
+  defp version_match?(version, req), do: Version.match?(version, req)
+
+  defp get_versions(package) do
+    package_vsn_ix = Package.__record__(:index, :version) + 1
+    :ets.lookup_element(@ets_table, package, package_vsn_ix)
+      |> Enum.sort(&Version.gt?/2)
+  end
+
+  defp get_deps(package, version) do
+    ms = [{
+      Package[name: package, version: version, deps: :"$1"],
+      [],
+      [:"$1"] }]
+    deps = :ets.select(@ets_table, ms) |> Enum.first
+
+    Enum.map(deps, fn { name, req } ->
+      Request[name: name, req: req, parent: package]
+    end)
+  end
+
+  defp wrap(nil), do: []
+  defp wrap(arg), do: [arg]
 end
--- test/explex_test.exs
+++ test/explex_test.exs
@@ -1,7 +1,105 @@
 defmodule ExplexTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
 
-  test "the truth" do
-    assert(true)
+  _packages = [
+    { :postgrex, "0.0.1", [] },
+    { :postgrex, "0.1.0", [] },
+    { :postgrex, "0.1.1", [] },
+    { :postgrex, "0.1.2", [] },
+    { :postgrex, "0.2.0", [] },
+    { :ecto, "0.0.1", [] },
+    { :ecto, "0.0.2", [] },
+    { :ecto, "0.1.0", [{ :postgrex, "~> 0.1.0"}] },
+    { :ecto, "0.1.2", [{ :postgrex, "~> 0.1.0"}] },
+    { :ecto, "0.2.0", [{ :postgrex, "~> 0.3.0"}] },
+  ]
+
+  defp insert_data(packages) do
+    Enum.map(packages, &Tuple.insert_at(&1, 0, Explex.Package))
+      |> Explex.add_packages
+  end
+
+  setup do
+    Explex.start
+    :ok
+  end
+
+  teardown do
+    Explex.stop
+    :ok
+  end
+
+  test "simple" do
+    insert_data [
+      { :postgrex, "0.0.1", [] },
+      { :postgrex, "0.1.0", [] },
+      { :postgrex, "0.2.0", [] },
+      { :postgrex, "0.2.1", [] },
+      { :ecto, "0.0.1", [] },
+      { :ecto, "0.1.0", [{ :postgrex, "~> 0.1.0"}] },
+      { :ecto, "0.2.0", [{ :postgrex, "~> 0.2.0"}] },
+    ]
+
+    deps = [postgrex: nil, ecto: nil]
+    assert [postgrex: "0.2.1", ecto: "0.2.0"] = Explex.resolve(deps)
+
+    deps = [postgrex: "0.2.1", ecto: "0.2.0"]
+    assert [postgrex: "0.2.1", ecto: "0.2.0"] = Explex.resolve(deps)
+
+    deps = [postgrex: "0.2.0", ecto: "0.2.0"]
+    assert [postgrex: "0.2.0", ecto: "0.2.0"] = Explex.resolve(deps)
+
+    deps = [postgrex: "~> 0.3.0", ecto: nil]
+    assert nil = Explex.resolve(deps)
+
+    deps = [postgrex: nil, ecto: "~> 0.3.0"]
+    assert nil = Explex.resolve(deps)
+  end
+
+  test "backtrack" do
+    insert_data [
+      { :postgrex, "0.0.1", [] },
+      { :postgrex, "0.1.0", [] },
+      { :postgrex, "0.2.0", [] },
+      { :postgrex, "0.2.1", [] },
+      { :ecto, "0.0.1", [] },
+      { :ecto, "0.0.2", [{ :postgrex, "0.1.1"}] },
+      { :ecto, "0.1.0", [{ :postgrex, "~> 0.1.0"}] },
+      { :ecto, "0.1.2", [{ :postgrex, "~> 0.1.0"}] },
+      { :ecto, "0.2.0", [{ :postgrex, "~> 0.2.0"}] },
+    ]
+
+    deps = [postgrex: "0.2.0", ecto: "0.2.0"]
+    assert [postgrex: "0.2.0", ecto: "0.2.0"] = Explex.resolve(deps)
+
+    deps = [postgrex: "0.1.0", ecto: ">= 0.1.0"]
+    assert [postgrex: "0.1.0", ecto: "0.1.2"] = Explex.resolve(deps)
+
+    deps = [postgrex: nil, ecto: "< 0.1.0"]
+    assert [postgrex: "0.2.1", ecto: "0.0.1"] = Explex.resolve(deps)
+
+    deps = [postgrex: "0.1.0", ecto: "< 0.1.0"]
+    assert [postgrex: "0.1.0", ecto: "0.0.1"] = Explex.resolve(deps)
+
+    deps = [postgrex: "0.1.0", ecto: "~> 0.0.2"]
+    assert nil = Explex.resolve(deps)
+
+    deps = [postgrex: nil, ecto: "0.0.2"]
+    assert nil = Explex.resolve(deps)
+  end
+
+  test "complete backtrack" do
+    insert_data [
+      { :postgrex, "0.2.0", [] },
+      { :postgrex, "0.2.1", [] },
+      { :ecto, "0.0.1", [] },
+      { :ecto, "0.0.2", [] },
+      { :ecto, "0.1.0", [{ :postgrex, "~> 0.1.0"}] },
+      { :ecto, "0.1.2", [{ :postgrex, "~> 0.1.0"}] },
+      { :ecto, "0.2.0", [{ :postgrex, "~> 0.3.0"}] },
+    ]
+
+    deps = [postgrex: nil, ecto: nil]
+    assert [postgrex: "0.2.1", ecto: "0.0.2"] = Explex.resolve(deps)
   end
 end
