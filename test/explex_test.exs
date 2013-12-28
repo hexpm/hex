1d2ee9abd525aa48d7eb742dde0b48db3ed61604
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2013-12-28 17:18:46 +0100

    More tests

--- test/explex_test.exs
+++ test/explex_test.exs
@@ -1,19 +1,5 @@
 defmodule ExplexTest do
   use ExUnit.Case, async: false
-
-  _packages = [
-    { :postgrex, "0.0.1", [] },
-    { :postgrex, "0.1.0", [] },
-    { :postgrex, "0.1.1", [] },
-    { :postgrex, "0.1.2", [] },
-    { :postgrex, "0.2.0", [] },
-    { :ecto, "0.0.1", [] },
-    { :ecto, "0.0.2", [] },
-    { :ecto, "0.1.0", [{ :postgrex, "~> 0.1.0"}] },
-    { :ecto, "0.1.2", [{ :postgrex, "~> 0.1.0"}] },
-    { :ecto, "0.2.0", [{ :postgrex, "~> 0.3.0"}] },
-  ]
-
   defp insert_data(packages) do
     Enum.map(packages, &Tuple.insert_at(&1, 0, Explex.Package))
       |> Explex.add_packages
@@ -36,18 +22,18 @@ defmodule ExplexTest do
       { :postgrex, "0.2.0", [] },
       { :postgrex, "0.2.1", [] },
       { :ecto, "0.0.1", [] },
-      { :ecto, "0.1.0", [{ :postgrex, "~> 0.1.0"}] },
-      { :ecto, "0.2.0", [{ :postgrex, "~> 0.2.0"}] },
+      { :ecto, "0.1.0", [postgrex: "~> 0.1.0"] },
+      { :ecto, "0.2.0", [postgrex: "~> 0.2.0"] },
     ]
 
     deps = [postgrex: nil, ecto: nil]
-    assert [postgrex: "0.2.1", ecto: "0.2.0"] = Explex.resolve(deps)
+    assert Dict.equal? [postgrex: "0.2.1", ecto: "0.2.0"], Explex.resolve(deps)
 
     deps = [postgrex: "0.2.1", ecto: "0.2.0"]
-    assert [postgrex: "0.2.1", ecto: "0.2.0"] = Explex.resolve(deps)
+    assert Dict.equal? [postgrex: "0.2.1", ecto: "0.2.0"], Explex.resolve(deps)
 
     deps = [postgrex: "0.2.0", ecto: "0.2.0"]
-    assert [postgrex: "0.2.0", ecto: "0.2.0"] = Explex.resolve(deps)
+    assert Dict.equal? [postgrex: "0.2.0", ecto: "0.2.0"], Explex.resolve(deps)
 
     deps = [postgrex: "~> 0.3.0", ecto: nil]
     assert nil = Explex.resolve(deps)
@@ -63,23 +49,23 @@ defmodule ExplexTest do
       { :postgrex, "0.2.0", [] },
       { :postgrex, "0.2.1", [] },
       { :ecto, "0.0.1", [] },
-      { :ecto, "0.0.2", [{ :postgrex, "0.1.1"}] },
-      { :ecto, "0.1.0", [{ :postgrex, "~> 0.1.0"}] },
-      { :ecto, "0.1.2", [{ :postgrex, "~> 0.1.0"}] },
-      { :ecto, "0.2.0", [{ :postgrex, "~> 0.2.0"}] },
+      { :ecto, "0.0.2", [postgrex: "0.1.1"] },
+      { :ecto, "0.1.0", [postgrex: "~> 0.1.0"] },
+      { :ecto, "0.1.2", [postgrex: "~> 0.1.0"] },
+      { :ecto, "0.2.0", [postgrex: "~> 0.2.0"] },
     ]
 
     deps = [postgrex: "0.2.0", ecto: "0.2.0"]
-    assert [postgrex: "0.2.0", ecto: "0.2.0"] = Explex.resolve(deps)
+    assert Dict.equal? [postgrex: "0.2.0", ecto: "0.2.0"], Explex.resolve(deps)
 
     deps = [postgrex: "0.1.0", ecto: ">= 0.1.0"]
-    assert [postgrex: "0.1.0", ecto: "0.1.2"] = Explex.resolve(deps)
+    assert Dict.equal? [postgrex: "0.1.0", ecto: "0.1.2"], Explex.resolve(deps)
 
     deps = [postgrex: nil, ecto: "< 0.1.0"]
-    assert [postgrex: "0.2.1", ecto: "0.0.1"] = Explex.resolve(deps)
+    assert Dict.equal? [postgrex: "0.2.1", ecto: "0.0.1"], Explex.resolve(deps)
 
     deps = [postgrex: "0.1.0", ecto: "< 0.1.0"]
-    assert [postgrex: "0.1.0", ecto: "0.0.1"] = Explex.resolve(deps)
+    assert Dict.equal? [postgrex: "0.1.0", ecto: "0.0.1"], Explex.resolve(deps)
 
     deps = [postgrex: "0.1.0", ecto: "~> 0.0.2"]
     assert nil = Explex.resolve(deps)
@@ -94,12 +80,27 @@ defmodule ExplexTest do
       { :postgrex, "0.2.1", [] },
       { :ecto, "0.0.1", [] },
       { :ecto, "0.0.2", [] },
-      { :ecto, "0.1.0", [{ :postgrex, "~> 0.1.0"}] },
-      { :ecto, "0.1.2", [{ :postgrex, "~> 0.1.0"}] },
-      { :ecto, "0.2.0", [{ :postgrex, "~> 0.3.0"}] },
+      { :ecto, "0.1.0", [postgrex: "~> 0.1.0"] },
+      { :ecto, "0.1.2", [postgrex: "~> 0.1.0"] },
+      { :ecto, "0.2.0", [postgrex: "~> 0.3.0"] },
     ]
 
     deps = [postgrex: nil, ecto: nil]
-    assert [postgrex: "0.2.1", ecto: "0.0.2"] = Explex.resolve(deps)
+    assert Dict.equal? [postgrex: "0.2.1", ecto: "0.0.2"], Explex.resolve(deps)
+  end
+
+  test "more backtrack" do
+    insert_data [
+      { :ex_doc, "0.0.1", [] },
+      { :ex_doc, "0.0.2", [] },
+      { :ex_doc, "0.1.0", [] },
+      { :postgrex, "0.2.0", [ex_doc: "0.0.1"] },
+      { :postgrex, "0.2.1", [ex_doc: "~> 0.1.0"] },
+      { :ecto, "0.2.0", [postgrex: "~> 0.2.0", ex_doc: "~> 0.0.1"] },
+      { :ecto, "0.2.1", [postgrex: "~> 0.2.0", ex_doc: "0.0.2"] },
+    ]
+
+    deps = [ecto: nil]
+    assert Dict.equal? [ecto: "0.2.0", postgrex: "0.2.0", ex_doc: "0.0.1"], Explex.resolve(deps)
   end
 end
