7701fcae8b6532d876eb7a12e381f7870e156e86
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-03-07 11:38:14 +0100

    Fix search test

--- test/mix/tasks/hex/search_test.exs
+++ test/mix/tasks/hex/search_test.exs
@@ -3,7 +3,12 @@ defmodule Mix.Tasks.Hex.SearchTest do
   @moduletag :integration
 
   test "search" do
-    Mix.Tasks.Hex.Search.run(["e"])
-    assert_received { :mix_shell, :info, ["decimal"] }
+    System.put_env("MIX_HOME", tmp_path())
+    Mix.Tasks.Hex.Search.run(["ex"])
+    assert_received { :mix_shell, :info, ["ex_doc"] }
+    assert_received { :mix_shell, :info, ["ex_plex"] }
+    assert_received { :mix_shell, :info, ["postgrex"] }
+  after
+    System.delete_env("MIX_HOME")
   end
 end
