0eefb83cd72c47af0468a71d4f83bec2ca62617d
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-09-06 20:14:31 +0200

    Bump to v0.4.4-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Hex.Mixfile do
 
   def project do
     [app: :hex,
-     version: "0.4.3",
+     version: "0.4.4-dev",
      elixir: "~> 1.0.0-rc1",
      aliases: aliases,
      deps: deps]
