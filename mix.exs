477e614e237983b71937e582a635df2001a20fa1
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-12-11 00:54:41 +0100

    Bump to v0.6.2-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Hex.Mixfile do
 
   def project do
     [app: :hex,
-     version: "0.6.1",
+     version: "0.6.2-dev",
      aliases: aliases,
      deps: deps,
      elixirc_options: elixirc_options(Mix.env)]
