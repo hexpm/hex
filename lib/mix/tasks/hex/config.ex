eb8de710d766b8cd3a7fca19e5a4d751be3c2e44
José Valim <jose.valim@gmail.com>
2015-10-16 21:05:16 +0200

    Update config.ex

--- lib/mix/tasks/hex/config.ex
+++ lib/mix/tasks/hex/config.ex
@@ -6,15 +6,28 @@ defmodule Mix.Tasks.Hex.Config do
   @moduledoc """
   Reads or updates Hex configuration file.
 
-  `mix hex.config KEY [VALUE]`
+      mix hex.config KEY [VALUE]
 
-   ## Config keys
+  ## Config keys
+
     * `username` - Hex username
     * `key` - Hex API key
     * `api_url` - Hex API base URL (without trailing slash)
     * `cdn_url` - Hex CDN base URL (without trailing slash)
     * `http_proxy` - HTTP proxy server
     * `https_proxy` - HTTPS proxy server
+  
+  ## CDN
+
+  Hex provides 3 different CDNs for the following areas:
+  
+    * Americas (default) - https://s3.amazonaws.com/s3.hex.pm
+    * Europe, Africa - https://s3-eu-west-1.amazonaws.com/s3-eu.hex.pm
+    * Asia, Oceania - https://s3-ap-southeast-1.amazonaws.com/s3-asia.hex.pm
+  
+  You can change the default CDN as follows:
+  
+      mix hex.config cdn_url https://s3-eu-west-1.amazonaws.com/s3-eu.hex.pm
   """
 
   def run(args) do
