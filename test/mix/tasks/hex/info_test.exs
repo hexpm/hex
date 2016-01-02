058791b15f4bc297ec46229b56d86b4656fbdc23
Aleksei Magusev <lexmag@me.com>
2016-01-02 02:03:10 +0100

    Refine hex.info task output

--- test/mix/tasks/hex/info_test.exs
+++ test/mix/tasks/hex/info_test.exs
@@ -4,10 +4,9 @@ defmodule Mix.Tasks.Hex.InfoTest do
 
   test "package" do
     Mix.Tasks.Hex.Info.run(["ex_doc"])
-    assert_received {:mix_shell, :info, ["ex_doc"]}
-    assert_received {:mix_shell, :info, ["  Config: {:ex_doc, \"~> 0.1.0\"}"]}
-    assert_received {:mix_shell, :info, ["  Maintainers: John Doe, Jane Doe"]}
-    assert_received {:mix_shell, :info, ["builds docs"]}
+    assert_received {:mix_shell, :info, ["Builds docs\n"]}
+    assert_received {:mix_shell, :info, ["Config: {:ex_doc, \"~> 0.1.0\"}"]}
+    assert_received {:mix_shell, :info, ["Maintainers: John Doe, Jane Doe"]}
 
     Mix.Tasks.Hex.Info.run(["no_package"])
     assert_received {:mix_shell, :error, ["No package with name no_package"]}
@@ -15,12 +14,10 @@ defmodule Mix.Tasks.Hex.InfoTest do
 
   test "release" do
     Mix.Tasks.Hex.Info.run(["ex_doc", "0.0.1"])
-    assert_received {:mix_shell, :info, ["ex_doc v0.0.1"]}
-    assert_received {:mix_shell, :info, ["  Config: {:ex_doc, \"~> 0.0.1\"}"]}
+    assert_received {:mix_shell, :info, ["Config: {:ex_doc, \"~> 0.0.1\"}"]}
 
     Mix.Tasks.Hex.Info.run(["ex_doc", "0.1.0-rc1"])
-    assert_received {:mix_shell, :info, ["ex_doc v0.1.0-rc1"]}
-    assert_received {:mix_shell, :info, ["  Config: {:ex_doc, \"~> 0.1.0-rc1\"}"]}
+    assert_received {:mix_shell, :info, ["Config: {:ex_doc, \"~> 0.1.0-rc1\"}"]}
 
     Mix.Tasks.Hex.Info.run(["ex_doc", "1.2.3"])
     assert_received {:mix_shell, :error, ["No release with name ex_doc v1.2.3"]}
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -19,7 +19,8 @@ unless :integration in ExUnit.configuration[:exclude] do
     "maintainers" => ["John Doe", "Jane Doe"],
     "licenses" => ["GPL2", "MIT", "Apache"],
     "links" => %{"docs" => "http://docs", "repo" => "http://repo"},
-    "description" => "builds docs"}
+    "description" => "Builds docs"
+  }
 
   auth = HexWeb.new_user("user", "user@mail.com", "hunter42", "my_key")
   package_name_meta = Map.put(pkg_meta, "app", "app_name")
