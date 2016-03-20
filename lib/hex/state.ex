88190bc6ed7f4a95d91aa32a4c2fb642febc02df
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-03-20 22:33:52 +0100

    Use https to fastly

--- lib/hex/state.ex
+++ lib/hex/state.ex
@@ -3,7 +3,7 @@ defmodule Hex.State do
   @logged_keys ~w(http_proxy HTTP_PROXY https_proxy HTTPS_PROXY)
   @default_home "~/.hex"
   @default_url "https://hex.pm/api"
-  @default_mirror "http://s3.hex.pm.global.prod.fastly.net"
+  @default_mirror "https://hexpmrepo.global.ssl.fastly.net"
 
   @hexpm_pk """
   -----BEGIN PUBLIC KEY-----
