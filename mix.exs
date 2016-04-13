0f1036117ea6911de8fec815794d1ee4558a33df
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-04-13 19:29:59 +0200

    Bump to 0.12.0-dev
    Due to major changes to the lock.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Hex.Mixfile do
 
   def project do
     [app: :hex,
-     version: "0.11.6-dev",
+     version: "0.12.0-dev",
      elixir: "~> 1.0",
      aliases: aliases,
      deps: deps,
