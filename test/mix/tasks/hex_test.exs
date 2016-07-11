23f2ec1eee7f6004876e5f6daa10dbca6653c5d7
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-07-11 17:33:59 +0200

    Add workaround for prune_deps in Elixir 1.4-dev

--- test/mix/tasks/hex_test.exs
+++ test/mix/tasks/hex_test.exs
@@ -2,8 +2,14 @@ defmodule Mix.Tasks.HexTest do
   use HexTest.Case
 
   test "run without args shows help" do
-    Mix.Tasks.Hex.run([])
-    assert_received {:mix_shell, :info, ["Hex is a package manager for the Erlang ecosystem."]}
+    # Workaround for prune_deps in Elixir 1.4-dev
+    paths = :code.get_path
+    try do
+      Mix.Tasks.Hex.run([])
+      assert_received {:mix_shell, :info, ["Hex is a package manager for the Erlang ecosystem."]}
+    after
+      true = :code.set_path(paths)
+    end
   end
 
   test "run with invalid arguments" do
