7fd5b4f4e336bb39b6c4b4758cf03d1c5b11e956
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-07-21 15:01:06 +0200

    Use MIX_NO_DEPS in tests

--- test/mix/tasks/hex_test.exs
+++ test/mix/tasks/hex_test.exs
@@ -2,13 +2,13 @@ defmodule Mix.Tasks.HexTest do
   use HexTest.Case
 
   test "run without args shows help" do
-    # Workaround for prune_deps in Elixir 1.4-dev
-    paths = :code.get_path
+    System.put_env("MIX_NO_DEPS", "1")
+
     try do
       Mix.Tasks.Hex.run([])
       assert_received {:mix_shell, :info, ["Hex is a package manager for the Erlang ecosystem."]}
     after
-      true = :code.set_path(paths)
+      System.put_env("MIX_NO_DEPS", "0")
     end
   end
 
