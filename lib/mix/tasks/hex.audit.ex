105de1926625f5bf8cc54e3818c190850677cbe0
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-07-22 03:38:50 +0700

    Add hex.audit (#381)

---
+++ lib/mix/tasks/hex.audit.ex
@@ -0,0 +1,57 @@
+defmodule Mix.Tasks.Hex.Audit do
+  use Mix.Task
+  alias Hex.Registry.Server, as: Registry
+
+  @shortdoc "Shows retired Hex dependencies"
+
+  @moduledoc """
+  Shows all Hex dependencies that have been marked as retired.
+
+  Retired packages are no longer recommended to be used by their
+  maintainers. The task will display a message describing
+  the reason for retirement and exit with a non-zero code
+  if any retired dependencies are found.
+  """
+
+  def run(_) do
+    Hex.start
+
+    lock = Mix.Dep.Lock.read()
+    deps = Mix.Dep.loaded([]) |> Enum.filter(& &1.scm == Hex.SCM)
+
+    Registry.open
+
+    Hex.Mix.packages_from_lock(lock)
+    |> Registry.prefetch
+
+    case retired_packages(deps, lock) do
+      [] ->
+        Hex.Shell.info "No retired packages found"
+      packages ->
+        header = ["Dependency", "Version", "Retirement reason"]
+        Mix.Tasks.Hex.print_table(header, packages)
+        Hex.Shell.error "Found retired packages"
+        set_exit_code(1)
+    end
+  end
+
+  defp retired_packages(deps, lock) do
+    Enum.map(deps, &retirement_status(Hex.Utils.lock(lock[&1.app])))
+    |> Enum.reject(&Enum.empty?/1)
+  end
+
+  defp retirement_status(%{repo: repo, name: package, version: version}) do
+    retired = Registry.retired(repo, package, version)
+
+    case retired do
+      %{} -> [package, version, Hex.Utils.package_retirement_message(retired)]
+      nil -> []
+    end
+  end
+
+  if Mix.env() == :test do
+    defp set_exit_code(_code), do: :ok
+  else
+    defp set_exit_code(code), do: System.at_exit(fn(_) -> System.halt(code) end)
+  end
+end
---
+++ test/mix/tasks/hex.audit_test.exs
@@ -0,0 +1,91 @@
+defmodule Mix.Tasks.Hex.AuditTest do
+  use HexTest.Case
+  @moduletag :integration
+
+  @package :test_package
+  @package_name Atom.to_string(@package)
+
+  defmodule RetiredDeps.Mixfile do
+    def project do
+      [app: :test_app,
+       version: "0.0.1",
+       deps: [{:test_package, ">= 0.1.0"}]]
+    end
+  end
+
+  setup_all do
+    auth = Hexpm.new_user("audit_user", "audit@mail.com", "passpass", "key")
+    {:ok, [auth: auth]}
+  end
+
+  test "audit (retired package without a message)", context do
+    with_test_package "0.1.0", context, fn ->
+      retire_test_package "0.1.0", "security"
+
+      Mix.Task.run "hex.audit"
+
+      assert_output_row @package_name, "0.1.0", "(security)"
+
+      assert_received {:mix_shell, :error, ["Found retired packages"]}
+    end
+  end
+
+  test "audit (retired package with a custom message)", context do
+    with_test_package "0.2.0", context, fn ->
+      retire_test_package "0.2.0", "invalid", "Superseded by v1.0.0"
+
+      Mix.Task.run "hex.audit"
+
+      assert_output_row @package_name, "0.2.0", "(invalid) Superseded by v1.0.0"
+
+      assert_received {:mix_shell, :error, ["Found retired packages"]}
+    end
+  end
+
+  test "audit (no retired packages)", context do
+    with_test_package "1.0.0", context, fn ->
+      Mix.Task.run "hex.audit"
+
+      assert_received {:mix_shell, :info, ["No retired packages found"]}
+    end
+  end
+
+  def with_test_package(version, %{auth: auth}, fun) do
+    Mix.Project.push RetiredDeps.Mixfile
+
+    Hexpm.new_package(@package_name, version, [], %{}, auth)
+
+    in_tmp fn ->
+      Hex.State.put(:home, tmp_path())
+      Hex.Config.update(auth)
+      Mix.Dep.Lock.write %{@package => {:hex, @package, version}}
+
+      Mix.Task.run "deps.get"
+      flush()
+
+      fun.()
+    end
+  end
+
+  defp retire_test_package(version, reason, message \\ "") do
+    send self(), {:mix_shell_input, :prompt, "passpass"}
+    Mix.Tasks.Hex.Retire.run([@package_name, version, reason, "--message", message])
+
+    # Mix does not support the RemoteConverger.post_converge/0 callback on Elixir < 1.4,
+    # so we need to explicitly reset the registry.
+    Hex.Registry.Server.close()
+  end
+
+  defp assert_output_row(package, version, message) do
+    whitespace_length = String.length("Retirement reason  ") - String.length(message)
+    whitespace_length = if whitespace_length < 2, do: 2, else: whitespace_length
+
+    output =
+      [package, :reset, "  ", version, :reset, "    ", message, :reset,
+        String.duplicate(" ", whitespace_length)]
+      |> IO.ANSI.format
+      |> List.to_string
+
+    assert_received {:mix_shell, :info, [^output]}
+  end
+end
