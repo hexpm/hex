abf87b686870e7fa2a1c51466c8573168d7b4ecc
Ashu Goel <asgoel13@gmail.com>
2017-08-01 00:33:02 -0700

    [hex.search] use package html_url (#400)

--- test/mix/tasks/hex.search_test.exs
+++ test/mix/tasks/hex.search_test.exs
@@ -6,8 +6,8 @@ defmodule Mix.Tasks.Hex.SearchTest do
     Mix.Tasks.Hex.Search.run(["doc"])
     assert_received {:mix_shell, :info, ["ex_doc\e[0m" <> ex_doc]}
     assert_received {:mix_shell, :info, ["only_doc\e[0m" <> only_doc]}
-    assert ex_doc =~ ~r"\w*0\.1\.0.*https://hex.pm/packages/ex_doc"
-    assert only_doc =~ ~r"\w*0\.1\.0.*https://hex.pm/packages/only_doc"
+    assert ex_doc =~ ~r"\w*0\.1\.0.*http://localhost:4043/packages/ex_doc"
+    assert only_doc =~ ~r"\w*0\.1\.0.*http://localhost:4043/packages/only_doc"
   end
 
   test "empty search" do
