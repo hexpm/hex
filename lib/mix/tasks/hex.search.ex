272d5fd5b72e93f5b6dd690cee3a9d0b7ec22324
Gustavo Villa <gfvcastro@gmail.com>
2017-09-04 21:00:02 -0300

    Add description to hex.search task

--- lib/mix/tasks/hex.search.ex
+++ lib/mix/tasks/hex.search.ex
@@ -39,13 +39,17 @@ defmodule Mix.Tasks.Hex.Search do
   defp lookup_packages({:ok, {200, packages, _headers}}) do
     values =
       Enum.map(packages, fn package ->
-        [package["name"], latest(package["releases"]), package["html_url"] || package["url"]]
+        [package["name"], Hex.Utils.truncate(package["meta"]["description"] |> trim_heredoc), latest(package["releases"]), package["html_url"] || package["url"]]
       end)
 
-    Mix.Tasks.Hex.print_table(["Package", "Version", "URL"], values)
+    Mix.Tasks.Hex.print_table(["Package", "Description", "Version", "URL"], values)
   end
 
   defp latest([%{"version" => version} | _]) do
     version
+  end
+
+  defp trim_heredoc(string) do
+    string |> String.split("\n", trim: true) |> Enum.map_join(" ", &(&1 |> Hex.string_trim()))
   end
 end
