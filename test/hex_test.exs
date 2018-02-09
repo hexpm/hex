15ef76f99725c93d0881427019f03459aeeea72b
Wojtek Mach <wojtek@wojtekmach.pl>
2018-02-09 22:28:39 +0100

    Refactor test

--- test/hex_test.exs
+++ test/hex_test.exs
@@ -1,10 +1,8 @@
 defmodule HexTest do
   use HexTest.Case
 
-  test "adapt organization to repo" do
-    opts = [organization: "acme"]
-    opts = Hex.organization_to_repo(opts)
-    refute Keyword.has_key?(opts, :organization)
-    assert "hexpm:acme" == Keyword.get(opts, :repo)
+  test "organization_to_repo/1" do
+    opts = Hex.organization_to_repo([organization: "acme"])
+    assert Keyword.fetch!(opts, :repo) == "hexpm:acme"
   end
 end
