5d8ba1916ec69f70b179403f80410b455cc7e686
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-05-25 18:22:51 +0200

    Remove unneccesary API.User.test/2

--- lib/hex/api/user.ex
+++ lib/hex/api/user.ex
@@ -9,10 +9,6 @@ defmodule Hex.API.User do
     API.request(:get, nil, "users/#{URI.encode(username)}")
   end
 
-  def test(username, auth) do
-    API.request(:get, nil, "users/#{URI.encode(username)}/test", auth)
-  end
-
   def new(username, email, password) do
     API.erlang_post_request(nil, "users", %{username: username, email: email, password: password})
   end
