f6ae207c0fb4fee85673a38a5ee5741c2be340c7
supersimple <toddresudek@gmail.com>
2018-05-26 12:35:30 -0600

    removes describe block to support older versions

--- test/mix/tasks/hex.owner_test.exs
+++ test/mix/tasks/hex.owner_test.exs
@@ -2,75 +2,71 @@ defmodule Mix.Tasks.Hex.OwnerTest do
   use HexTest.Case
   @moduletag :integration
 
-  describe "add_owner/4" do
-    test "add owner" do
-      auth = Hexpm.new_user("owner_user1", "owner_user1@mail.com", "passpass", "key")
-      Hexpm.new_user("owner_user2", "owner_user2@mail.com", "passpass", "key")
-      Hexpm.new_package("owner_package1", "0.0.1", [], %{}, auth)
+  test "add owner" do
+    auth = Hexpm.new_user("owner_user1", "owner_user1@mail.com", "passpass", "key")
+    Hexpm.new_user("owner_user2", "owner_user2@mail.com", "passpass", "key")
+    Hexpm.new_package("owner_package1", "0.0.1", [], %{}, auth)
 
-      Hex.State.put(:home, tmp_path())
-      Mix.Tasks.Hex.update_keys(auth[:"$write_key"], auth[:"$read_key"])
+    Hex.State.put(:home, tmp_path())
+    Mix.Tasks.Hex.update_keys(auth[:"$write_key"], auth[:"$read_key"])
 
-      send(self(), {:mix_shell_input, :prompt, "passpass"})
-      Mix.Tasks.Hex.Owner.run(["add", "owner_package1", "owner_user2@mail.com"])
+    send(self(), {:mix_shell_input, :prompt, "passpass"})
+    Mix.Tasks.Hex.Owner.run(["add", "owner_package1", "owner_user2@mail.com"])
 
-      assert_received {:mix_shell, :info,
-                       [
-                         "Adding owner owner_user2@mail.com with ownership level full to owner_package1"
-                       ]}
+    assert_received {:mix_shell, :info,
+                     [
+                       "Adding owner owner_user2@mail.com with ownership level full to owner_package1"
+                     ]}
 
-      assert {:ok, {200, %{"owned_packages" => %{"owner_package1" => _}}, _}} =
-               Hex.API.User.get("owner_user2")
-    end
+    assert {:ok, {200, %{"owned_packages" => %{"owner_package1" => _}}, _}} =
+             Hex.API.User.get("owner_user2")
+  end
 
-    test "add owner with maintainer level" do
-      auth = Hexpm.new_user("owner_user1a", "owner_user1a@mail.com", "passpass", "key")
-      Hexpm.new_user("owner_user2a", "owner_user2a@mail.com", "passpass", "key")
-      Hexpm.new_package("owner_package1a", "0.0.1", [], %{}, auth)
+  test "add owner with maintainer level" do
+    auth = Hexpm.new_user("owner_user1a", "owner_user1a@mail.com", "passpass", "key")
+    Hexpm.new_user("owner_user2a", "owner_user2a@mail.com", "passpass", "key")
+    Hexpm.new_package("owner_package1a", "0.0.1", [], %{}, auth)
 
-      Hex.State.put(:home, tmp_path())
-      Mix.Tasks.Hex.update_keys(auth[:"$write_key"], auth[:"$read_key"])
+    Hex.State.put(:home, tmp_path())
+    Mix.Tasks.Hex.update_keys(auth[:"$write_key"], auth[:"$read_key"])
 
-      send(self(), {:mix_shell_input, :prompt, "passpass"})
+    send(self(), {:mix_shell_input, :prompt, "passpass"})
 
+    Mix.Tasks.Hex.Owner.run([
+      "add",
+      "owner_package1a",
+      "owner_user2a@mail.com",
+      "--level",
+      "maintainer"
+    ])
+
+    assert_received {:mix_shell, :info,
+                     [
+                       "Adding owner owner_user2a@mail.com with ownership level maintainer to owner_package1a"
+                     ]}
+
+    assert {:ok, {200, %{"owned_packages" => %{"owner_package1a" => _}}, _}} =
+             Hex.API.User.get("owner_user2a")
+  end
+
+  test "add owner with invalid level" do
+    auth = Hexpm.new_user("owner_user1b", "owner_user1b@mail.com", "passpass", "key")
+    Hexpm.new_user("owner_user2b", "owner_user2b@mail.com", "passpass", "key")
+    Hexpm.new_package("owner_package1b", "0.0.1", [], %{}, auth)
+
+    Hex.State.put(:home, tmp_path())
+    Mix.Tasks.Hex.update_keys(auth[:"$write_key"], auth[:"$read_key"])
+
+    send(self(), {:mix_shell_input, :prompt, "passpass"})
+
+    assert_raise Mix.Error, "Invalid ownership level, expected one of:\nfull\nmaintainer\n", fn ->
       Mix.Tasks.Hex.Owner.run([
         "add",
-        "owner_package1a",
-        "owner_user2a@mail.com",
+        "owner_package1b",
+        "owner_user2b@mail.com",
         "--level",
-        "maintainer"
+        "invalid"
       ])
-
-      assert_received {:mix_shell, :info,
-                       [
-                         "Adding owner owner_user2a@mail.com with ownership level maintainer to owner_package1a"
-                       ]}
-
-      assert {:ok, {200, %{"owned_packages" => %{"owner_package1a" => _}}, _}} =
-               Hex.API.User.get("owner_user2a")
-    end
-
-    test "add owner with invalid level" do
-      auth = Hexpm.new_user("owner_user1b", "owner_user1b@mail.com", "passpass", "key")
-      Hexpm.new_user("owner_user2b", "owner_user2b@mail.com", "passpass", "key")
-      Hexpm.new_package("owner_package1b", "0.0.1", [], %{}, auth)
-
-      Hex.State.put(:home, tmp_path())
-      Mix.Tasks.Hex.update_keys(auth[:"$write_key"], auth[:"$read_key"])
-
-      send(self(), {:mix_shell_input, :prompt, "passpass"})
-
-      assert_raise Mix.Error,
-                   "Invalid ownership level, expected one of:\nfull\nmaintainer\n",
-                   fn ->
-                     Mix.Tasks.Hex.Owner.run([
-                       "add",
-                       "owner_package1b",
-                       "owner_user2b@mail.com",
-                       "--level",
-                       "invalid"
-                     ])
-                   end
     end
   end
 
