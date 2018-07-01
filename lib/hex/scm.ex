88351b4907c437574ff0321fb6029f7b40474e36
Wojtek Mach <wojtek@wojtekmach.pl>
2018-07-01 13:17:00 +0200

    Dashes all the way down

--- lib/hex/scm.ex
+++ lib/hex/scm.ex
@@ -262,7 +262,7 @@ defmodule Hex.SCM do
   end
 
   defp cache_path(repo, name) do
-    repo = String.replace(repo, ":", "_")
+    repo = String.replace(repo, ":", "-")
     Path.join([Hex.State.fetch!(:home), @packages_dir, repo, name])
   end
 
