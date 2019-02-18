fcdee3b5c0e9dad4905e8bcffd57bae43da3f9f4
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2019-02-18 13:24:33 +0100

    Improve revert docs
    See #669.

--- test/mix/tasks/hex.publish_test.exs
+++ test/mix/tasks/hex.publish_test.exs
@@ -179,7 +179,7 @@ defmodule Mix.Tasks.Hex.PublishTest do
       assert_received {:mix_shell, :error, [^message]}
 
       send(self(), {:mix_shell_input, :prompt, "hunter42"})
-      Mix.Tasks.Hex.Publish.run(["--revert", "0.1.1"])
+      Mix.Tasks.Hex.Publish.run(["docs", "--revert", "0.1.1"])
       assert_received {:mix_shell, :info, ["Docs do not exist"]}
     end)
   end
