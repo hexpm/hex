9c2d798d2b9d81a959f8ca19a090b227b2c4c372
Dennis Beatty <dennis@dcbeatty.com>
2020-03-16 12:31:55 -0600

    clarify publish messaging around ownership

--- lib/mix/tasks/hex.publish.ex
+++ lib/mix/tasks/hex.publish.ex
@@ -313,10 +313,10 @@ defmodule Mix.Tasks.Hex.Publish do
 
   defp do_print_owner_prompt(organizations) do
     Hex.Shell.info(
-      "You are a member of organizations, select if you wish to publish " <>
-        "the package with yourself as owner or an organization as owner. " <>
+      "You are a member of organizations. Would you like to publish " <>
+        "the package with yourself as owner or an organization as owner? " <>
         "If you publish with an organization as owner your package will " <>
-        "be public but managed by selected the organization."
+        "be public but managed by the selected organization."
     )
 
     Hex.Shell.info("")
