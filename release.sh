e31878dbaeca4f474166b2d1a2e0ecbec1f3fa68
Wojtek Mach <wojtekmach@users.noreply.github.com>
2020-10-21 11:33:36 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -83,6 +83,3 @@ upload  "${hex_version}" 1.0.0 1.1.0 1.2.0 1.3.0 1.4.0 1.5.0 1.6.0 1.7.0 1.8.0 1
 pushd "../hexpm-ops"
 scripts/kubeexec hexpm --prod -- bin/hexpm eval "Hexpm.ReleaseTasks.script([\"add_install.exs\",\"${hex_version}\",\"1.11.0\",\"1.10.0\",\"1.9.0\",\"1.8.0\",\"1.7.0\",\"1.6.0\",\"1.5.0\",\"1.4.0\",\"1.3.0\",\"1.2.0\",\"1.1.0\",\"1.0.0\"])"
 popd
-
-curl --fail -XPURGE https://repo.hex.pm/installs/hex-1.x.csv
-curl --fail -XPURGE https://repo.hex.pm/installs/hex-1.x.csv.signed
