cbebc07d13178c25d2b6add77803f9346dff82d9
Todd Resudek <toddresudek@gmail.com>
2021-01-12 22:22:35 -0700

    adds tests for mix_xdg (skipped when otp < 19)

--- test/hex/config_test.exs
+++ test/hex/config_test.exs
@@ -14,7 +14,7 @@ defmodule Hex.ConfigTest do
     System.delete_env("HEX_HOME")
   end
 
-  @tag skip: System.otp_release() < "19"
+  @tag skip: System.version() < "1.3" and System.otp_release() < "19"
   test "find_config_home/1 when MIX_XDG is set and HEX_HOME is not" do
     System.delete_env("HEX_HOME")
     System.put_env("MIX_XDG", "true")
