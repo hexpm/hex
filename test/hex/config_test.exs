ba4b716a1825393793a1011cc6a43f1991e57cbd
Todd Resudek <toddresudek@gmail.com>
2021-01-12 22:07:22 -0700

    adds tests for mix_xdg (skipped when otp < 19)

--- test/hex/config_test.exs
+++ test/hex/config_test.exs
@@ -13,4 +13,21 @@ defmodule Hex.ConfigTest do
     assert Config.find_config_home(:user_cache) == {:ok, "/sys/tmp"}
     System.delete_env("HEX_HOME")
   end
+
+  @tag skip: System.otp_release() < "19"
+  test "find_config_home/1 when MIX_XDG is set and HEX_HOME is not" do
+    System.delete_env("HEX_HOME")
+    System.put_env("MIX_XDG", "true")
+    {:ok, dir} = Config.find_config_home(:user_cache)
+    assert dir =~ "/hex"
+    System.delete_env("MIX_XDG")
+  end
+
+  test "find_config_home/1 when MIX_XDG is set and HEX_HOME is set" do
+    System.put_env("MIX_XDG", "true")
+    System.put_env("HEX_HOME", "/sys/tmp")
+    assert Config.find_config_home(:user_cache) == {:ok, "/sys/tmp"}
+    System.delete_env("HEX_HOME")
+    System.delete_env("MIX_XDG")
+  end
 end
