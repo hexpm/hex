278bcc55fc43d232e3357f9e4126958b646a6958
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-01-15 09:26:44 +0000

    Unload hex application to fix compatability between Hex versions
    Starting hex <0.21 from mix.exs crashes if hex 0.21+ is already loaded.
    In the past we unloaded all current BEAM modules so that the development
    version of Hex can be started when Hex is installed on your system.
    We did not unload the application so when the application :mod key
    changed we crashed. Fix by unloading and reloading the application as
    well as the BEAM modules.

--- mix.exs
+++ mix.exs
@@ -80,6 +80,7 @@ defmodule Hex.MixProject do
 
   defp unload_hex(_) do
     Application.stop(:hex)
+    Application.unload(:hex)
     paths = Path.wildcard(Path.join(archives_path(), "hex*"))
 
     Enum.each(paths, fn archive ->
