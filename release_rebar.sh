e3ef43ae42b9df1c0ca87f032478891714dd5278
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-06-07 11:10:12 +0200

    Update rebar release script

--- release_rebar.sh
+++ release_rebar.sh
@@ -6,17 +6,24 @@ set -e -u
 # $2 = rebar version
 # $3 = elixir version
 function rebar_csv {
-  s3down "${1}-1.x.csv" "${1}-1.x.csv"
+  if [ -f "rebar3-1.x.csv" ]; then
+    mv rebar3-1.x.csv rebar3-1.x-old.csv
+  else
+    s3down "${1}-1.x.csv" "${1}-1.x-old.csv"
+  fi
 
   # Remove existing build for this elixir version
-  csv=$(cat ${1}-1.x.csv)
-  echo $csv | grep -v ",${3}\$" > ${1}-1.x.csv
+  cat ${1}-1.x-old.csv | grep -v ",${3}\$" > ${1}-1.x.csv
+  rm ${1}-1.x-old.csv
 
   # Add new build
   sha=$(shasum -a 512 "${1}")
   sha=($sha)
   echo "${2},${sha},${3}" >> ${1}-1.x.csv
+}
 
+# $1 = rebar name
+function sign_csv {
   openssl dgst -sha512 -sign "${ELIXIR_PEM}" "${1}-1.x.csv" | openssl base64 > ${1}-1.x.csv.signed
 }
 
@@ -46,10 +53,11 @@ function upload {
 
 # $1 = rebar version
 # $2 = otp version
+# $3 = ubuntu version
 function build_rebar2 {
   docker rm rebar2 || true
 
-  docker run --name rebar2 hexpm/erlang:$-ubuntu-xenial-20201014 sh -c "\
+  docker run --name rebar2 hexpm/erlang:$2-ubuntu-$3 sh -c "\
     apt update && apt -y install git && \
     git clone https://github.com/rebar/rebar.git -b $1 && \
     cd rebar && \
@@ -61,10 +69,11 @@ function build_rebar2 {
 
 # $1 = rebar version
 # $2 = otp version
+# $3 = ubuntu version
 function build_rebar3 {
   docker rm rebar3 || true
 
-  docker run --name rebar3 hexpm/erlang:$2-ubuntu-xenial-20201014 sh -c "\
+  docker run --name rebar3 hexpm/erlang:$2-ubuntu-$3 sh -c "\
     apt update && apt -y install git && \
     git clone https://github.com/erlang/rebar3.git -b $1 && \
     cd rebar3 && \
@@ -74,21 +83,35 @@ function build_rebar3 {
   docker rm rebar3
 }
 
-# Update these values for every release
-# Call as ELIXIR_PEM=elixir.pem ./release_rebar.sh rebar3 3.14.4
+rm rebar rebar3 rebar-1.x.csv rebar-1.x.csv.signed
 
-rebar_name=$1 # rebar / rebar3
-rebar_version=$2
+# Update these values for every release
+# Call as ELIXIR_PEM=elixir.pem ./release_rebar.sh
+
+# Note that the order of versions in the CSV file matters so all versions
+# of rebar needs to be updated.
+
+# For Elixir 1.0.0 / rebar 3.13.3
+rebar_name="rebar3"
+rebar_version="3.13.3"
+elixir_version="1.0.0"
+otp_version="17.5.6.10"
+ubuntu_version="xenial-20200326"
+build_rebar3 "${rebar_version}" "${otp_version}" "${ubuntu_version}"
+rebar_csv rebar3 "${rebar_version}" "${elixir_version}"
+
+# For Elixir 1.11.4 / rebar 3.15.2
+rebar_name="rebar3"
+rebar_version="3.15.2"
 elixir_version="1.11.4"
 otp_version="21.3.8.21"
+ubuntu_version="xenial-20201014"
+build_rebar3 "${rebar_version}" "${otp_version}" "${ubuntu_version}"
+rebar_csv rebar3 "${rebar_version}" "${elixir_version}"
 
-if [ "${rebar_name}" = "rebar" ]; then
-  build_rebar2 "${rebar_version}" "${otp_version}"
-fi
+sign_csv rebar3
+upload rebar3 "${rebar_version}" "${elixir_version}"
 
-if [ "${rebar_name}" = "rebar3" ]; then
-  build_rebar3 "${rebar_version}" "${otp_version}"
-fi
-
-rebar_csv "${rebar_name}" "${rebar_version}" "${elixir_version}"
-upload    "${rebar_name}" "${rebar_version}" "${elixir_version}"
+# build_rebar2 "${rebar_version}" "${otp_version}" "${ubuntu_version}"
+# rebar_csv rebar2 "${rebar_version}" "${elixir_version}"
+# upload rebar2 "${rebar_version}" "${elixir_version}"
