3a5f27f3ce78cfe2afc3ce2d0bcd35908ac85ed7
Frerich Raabe <frerich.raabe@betterdoc.de>
2021-12-02 19:34:00 +0100

    Introduce Hex.Netrc.lookup function
    This is the entry point to the .netrc-based login credential resolution.
    
    Given a host name and an (optional) path to a .netrc file, it tries to
    lookup login credentials to be used for that host.

---
+++ test/hex/netrc_test.exs
@@ -0,0 +1,40 @@
+defmodule Hex.NetrcTest do
+  use HexTest.Case, async: false
+
+  alias Hex.Netrc
+
+  setup do
+    # Restart Hex between tests to get a fresh cache.
+    Application.stop(:hex)
+    :ok = Application.start(:hex)
+  end
+
+  test "lookup/2 returns nil for unknown host" do
+    in_tmp(fn ->
+      data = """
+      machine foo.example.com
+        login foo
+        password bar
+      """
+
+      File.write!(".netrc", data)
+
+      assert {:ok, nil} = Netrc.lookup("bar.example.com", ".netrc")
+    end)
+  end
+
+  test "lookup/2 returns expected result for known host" do
+    in_tmp(fn ->
+      data = """
+      machine foo.example.com
+        login foo
+        password bar
+      """
+
+      File.write!(".netrc", data)
+
+      assert {:ok, %{username: "foo", password: "bar"}} =
+               Netrc.lookup("foo.example.com", ".netrc")
+    end)
+  end
+end
