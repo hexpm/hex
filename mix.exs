f2edd267bbaff2db8f534bf4bc04d5405fb27e26
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-12-13 21:42:59 +0100

    Bump v1.0.1-dev

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Hex.MixProject do
   use Mix.Project
 
-  @version "1.0.0"
+  @version "1.0.1-dev"
 
   {:ok, system_version} = Version.parse(System.version())
   @elixir_version {system_version.major, system_version.minor, system_version.patch}
