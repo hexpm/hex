e645e5c77d7d8dff0e8226a228ee0067405aa147
Benjamin Philip <benjamin.philip495@gmail.com>
2021-12-21 16:30:40 +0530

    Fix API.WebAuth functions

--- lib/hex/api/web_auth.ex
+++ lib/hex/api/web_auth.ex
@@ -4,7 +4,10 @@ defmodule Hex.API.WebAuth do
   alias Hex.API
 
   def get_code(key_name) do
-    API.request(:post, nil, "login/web_auth/code", %{key_name: key_name})
+    nil
+    |> API.erlang_post_request("web_auth/code", %{key_name: key_name})
+    |> elem(1)
+    |> elem(1)
   end
 
   def submit_in_browser() do
@@ -12,32 +15,37 @@ defmodule Hex.API.WebAuth do
       "Open link in browser?"
       |> Hex.Shell.format()
       |> Hex.Shell.yes?()
-      |> open("https://hex.pm/login/web_auth")
+      |> open()
     end)
   end
 
   def access_key(device_code) do
     Hex.API.check_write_api()
 
-    fn x -> access_key_task(x) end
+    fn -> access_key_task(%{device_code: device_code}) end
     |> Task.async()
-    |> Task.await(%{device_code: device_code}, 5 * 60 * 1000)
+    |> Task.await(5 * 60 * 1000)
   end
 
   defp access_key_task(params) do
-    case API.request(:post, nil, "login/web_auth/access_key", params) do
+    case API.erlang_post_request(nil, "web_auth/access_key", params) do
       {:ok, {_code, %{"write_key" => write_key, "read_key" => read_key}, _headers}} ->
         %{write_key: write_key, read_key: read_key}
 
-      {:ok, {_code, %{"error" => "request to be verified"}, _headers}} ->
+      {:ok, {_code, %{"message" => "request to be verified"}, _headers}} ->
         access_key_task(params)
     end
   end
 
-  defp open(bool, url)
-  defp open(false, _url), do: :ok
+  defp open(bool)
+  defp open(false), do: :ok
 
-  defp open(true, url) do
+  defp open(true) do
+    url =
+      :api_url
+      |> Hex.State.fetch!()
+      |> String.replace_suffix("api", "login/web_auth")
+
     {cmd, args} =
       case :os.type() do
         {:unix, :darwin} -> {"open", [url]}
