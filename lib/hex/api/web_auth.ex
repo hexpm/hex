e9c73cfba50a047306e83c9307de684f918123c5
Benjamin Philip <benjamin.philip495@gmail.com>
2022-02-11 15:18:55 +0530

    Add timeout for fast access_key_task requests
    We don't want to try send an access_key request more than once per
    second. Therefore, this commit adds support for a timeout after a
    request, which will be used if a request completes in less than a
    second.

--- lib/hex/api/web_auth.ex
+++ lib/hex/api/web_auth.ex
@@ -13,18 +13,24 @@ defmodule Hex.API.WebAuth do
   def access_key(device_code) do
     Hex.API.check_write_api()
 
-    fn -> access_key_task(%{device_code: device_code}) end
+    fn -> access_key_task(%{device_code: device_code}, DateTime.utc_now()) end
     |> Task.async()
     |> Task.await(5 * 60 * 1000)
   end
 
-  defp access_key_task(params) do
+  defp access_key_task(params, last_request_time) do
     case API.erlang_post_request(nil, "web_auth/access_key", params) do
       {:ok, {_code, %{"write_key" => write_key, "read_key" => read_key}, _headers}} ->
         %{write_key: write_key, read_key: read_key}
 
       {:ok, {_code, %{"message" => "request to be verified"}, _headers}} ->
-        access_key_task(params)
+        diff = DateTime.diff(DateTime.utc_now(), last_request_time)
+
+        if diff < 1000 do
+          Process.sleep(1000 - diff)
+        end
+
+        access_key_task(params, DateTime.utc_now())
     end
   end
 end
