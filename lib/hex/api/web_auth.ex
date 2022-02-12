7ccbe7b6da4203e88e3e97a8edae5425f428ac92
Benjamin Philip <benjamin.philip495@gmail.com>
2022-02-12 15:16:14 +0530

    Use hand written timeout instead of Task.await to access_key

--- lib/hex/api/web_auth.ex
+++ lib/hex/api/web_auth.ex
@@ -13,24 +13,27 @@ defmodule Hex.API.WebAuth do
   def access_key(device_code) do
     Hex.API.check_write_api()
 
-    fn -> access_key_task(%{device_code: device_code}, DateTime.utc_now()) end
-    |> Task.async()
-    |> Task.await(5 * 60 * 1000)
+    now = System.os_time(:millisecond)
+    access_key(%{device_code: device_code}, now, now + 5 * 60 * 1000)
   end
 
-  defp access_key_task(params, last_request_time) do
+  defp access_key(params, last_request_time, timeout_time) do
     case API.erlang_post_request(nil, "web_auth/access_key", params) do
       {:ok, {_code, %{"write_key" => write_key, "read_key" => read_key}, _headers}} ->
         %{write_key: write_key, read_key: read_key}
 
       {:ok, {_code, %{"message" => "request to be verified"}, _headers}} ->
-        diff = DateTime.diff(DateTime.utc_now(), last_request_time)
+        diff = System.os_time(:millisecond) - last_request_time
 
         if diff < 1000 do
           Process.sleep(1000 - diff)
         end
 
-        access_key_task(params, DateTime.utc_now())
+        access_key(params, System.os_time(:millisecond), timeout_time)
     end
+  end
+
+  defp access_key(_, last_request_time, timeout_time) when timeout_time > last_request_time do
+    raise "Browser-based authentication has timed out"
   end
 end
