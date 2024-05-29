5539fdefd3d462889e458a7aea245eb32308eb1b
Todd Resudek <toddresudek@gmail.com>
2024-05-28 19:40:54 -0600

    This adds valid licenses for test.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -17,7 +17,7 @@ unless :integration in ExUnit.configuration()[:exclude] do
   Hexpm.start()
 
   pkg_meta = %{
-    "licenses" => ["GPL2", "MIT", "Apache"],
+    "licenses" => ["GPL-2.0", "MIT", "Apache-2.0"],
     "links" => %{"docs" => "http://docs", "repo" => "http://repo"},
     "description" => "Some description"
   }
