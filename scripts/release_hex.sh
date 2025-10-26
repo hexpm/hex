13da4309f9c60061efc48f69da43ff183fb9efc4
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2025-10-26 16:17:15 +0100

    Update release script to include older Elixir versions (#1095)

--- scripts/release_hex.sh
+++ scripts/release_hex.sh
@@ -46,9 +46,9 @@ function main {
   build ${hex_version} 27.3.3    1.17.3 1.17.0 noble-20250404
 
   # Elixir v1.18
-  build ${hex_version} 25.3.2.16 1.18.0 1.18.0 noble-20241015 # need to use exactly 1.18.0 and that requires older otp & ubuntu
-  build ${hex_version} 26.2.5.6  1.18.0 1.18.0 noble-20241015 # ditto
-  build ${hex_version} 27.2      1.18.0 1.18.0 noble-20241015 # ditto
+  build ${hex_version} 25.3.2.20 1.18.0 1.18.0 noble-20250404 # need to use exactly 1.18.0 and that requires older otp & ubuntu
+  build ${hex_version} 26.2.5.11 1.18.0 1.18.0 noble-20250404 # ditto
+  build ${hex_version} 27.3.3    1.18.0 1.18.0 noble-20250404 # ditto
 
   rm -rf _build
   rm "${hex_csv}.bak"
