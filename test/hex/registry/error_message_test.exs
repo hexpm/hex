acffae7d2a3ba7397f27dc1e605335b0bb77f2b3
Christopher Varjas <cvolunteer@proton.me>
2025-10-26 21:09:55 -0400

    refactor assertions

--- test/hex/registry/error_message_test.exs
+++ test/hex/registry/error_message_test.exs
@@ -25,11 +25,12 @@ defmodule Hex.Registry.ErrorMessageTest do
       result = {:ok, {404, [], %{}}}
       send_error_message(result, "hexpm", "nonexistent_package", false)
 
-      output = get_shell_output()
+      assert_received {:mix_shell, :error, [msg]}
+      assert msg =~ "Failed to fetch record for nonexistent_package from registry"
 
-      assert output =~ "Failed to fetch record for nonexistent_package from registry"
-      assert output =~ "The package nonexistent_package does not exist"
-      assert output =~ "Please verify the package name is spelled correctly"
+      assert_received {:mix_shell, :error, [msg]}
+      assert msg =~ "The package nonexistent_package does not exist"
+      assert msg =~ "Please verify the package name is spelled correctly"
     end
 
     test "displays helpful message for non-existent package with cache" do
@@ -38,14 +39,15 @@ defmodule Hex.Registry.ErrorMessageTest do
       result = {:ok, {404, [], %{}}}
       send_error_message(result, "hexpm", package_name, true)
 
-      output = get_shell_output()
+      assert_received {:mix_shell, :error, [msg]}
 
-      assert output =~
+      assert msg =~
                "Failed to fetch record for #{package_name} from registry (using cache instead)"
 
       # Still shows detailed diagnostics to help user understand the issue
-      assert output =~ "The package #{package_name} does not exist"
-      assert output =~ "Please verify the package name is spelled correctly"
+      assert_received {:mix_shell, :error, [msg]}
+      assert msg =~ "The package #{package_name} does not exist"
+      assert msg =~ "Please verify the package name is spelled correctly"
     end
   end
 
@@ -68,11 +70,10 @@ defmodule Hex.Registry.ErrorMessageTest do
         result
       )
 
-      output = get_shell_output()
-
-      assert output =~ "You don't have permission to access #{package_name}"
-      assert output =~ "the package is private and requires authentication"
-      assert output =~ "run 'mix hex.user auth' to authenticate"
+      assert_received {:mix_shell, :error, [msg]}
+      assert msg =~ "You don't have permission to access #{package_name}"
+      assert msg =~ "the package is private and requires authentication"
+      assert msg =~ "run 'mix hex.user auth' to authenticate"
     end
   end
 
@@ -99,12 +100,11 @@ defmodule Hex.Registry.ErrorMessageTest do
         result
       )
 
-      output = get_shell_output()
-
-      assert output =~ "You don't have permission to access #{package_name}"
-      assert output =~ "the package is private and you don't have the required permissions"
-      assert output =~ "Contact the package owner to request access"
-      assert output =~ "https://hex.pm/dashboard"
+      assert_received {:mix_shell, :error, [msg]}
+      assert msg =~ "You don't have permission to access #{package_name}"
+      assert msg =~ "the package is private and you don't have the required permissions"
+      assert msg =~ "Contact the package owner to request access"
+      assert msg =~ "https://hex.pm/dashboard"
     end
   end
 
@@ -114,33 +114,5 @@ defmodule Hex.Registry.ErrorMessageTest do
 
     Hex.Shell.error("Failed to fetch record for #{package_name} from registry#{cached_message}")
     Hex.Registry.Server.print_missing_package_diagnostics(repo, package, result)
-  end
-
-  defp get_shell_output do
-    # Use flush to collect all shell messages
-    ref = make_ref()
-
-    Hex.Shell.Process.flush(fn
-      {:mix_shell, _, [msg]} ->
-        send(self(), {:collected, ref, msg})
-
-      _ ->
-        :ok
-    end)
-
-    # Now collect all the messages we sent back to ourselves
-    collect_messages(ref, [])
-  end
-
-  defp collect_messages(ref, acc) do
-    receive do
-      {:collected, ^ref, msg} ->
-        collect_messages(ref, [msg | acc])
-    after
-      0 ->
-        acc
-        |> Enum.reverse()
-        |> Enum.join("\n")
-    end
   end
 end
