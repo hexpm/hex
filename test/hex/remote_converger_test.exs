755ef8230b3079af52697e8896366adb3e8a5bcb
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2026-04-18 11:59:53 -0700

    Limit deps.get auth preflight to org repos

--- test/hex/remote_converger_test.exs
+++ test/hex/remote_converger_test.exs
@@ -61,12 +61,130 @@ defmodule Hex.RemoteConvergerTest do
     end
   end
 
+  defmodule PublicDepsWithExpiredOAuth.MixProject do
+    def project do
+      [
+        app: :public_deps_with_expired_oauth,
+        version: "0.1.0",
+        deps: [
+          {:postgrex, "0.2.1"}
+        ]
+      ]
+    end
+  end
+
+  defmodule OrganizationDepsWithExpiredOAuth.MixProject do
+    def project do
+      [
+        app: :organization_deps_with_expired_oauth,
+        version: "0.1.0",
+        deps: [
+          {:private_prompt_pkg, "0.1.0", repo: "hexpm:remote_converger_org"}
+        ]
+      ]
+    end
+  end
+
+  defp with_project(project, fun) do
+    Mix.Project.push(project)
+
+    try do
+      fun.()
+    after
+      Mix.Project.pop()
+    end
+  end
+
+  defp store_expired_oauth_token do
+    Hex.OnceCache.clear(Hex.OAuth.RefreshCache)
+
+    Hex.OAuth.store_token(%{
+      "access_token" => "expired_access_token",
+      "refresh_token" => "invalid_refresh_token",
+      "expires_at" => System.system_time(:second) - 3600
+    })
+  end
+
+  defp new_repo_auth_user(prefix) do
+    suffix = System.unique_integer([:positive])
+
+    Hexpm.new_user(
+      "#{prefix}_#{suffix}",
+      "#{prefix}_#{suffix}@mail.com",
+      "password",
+      "#{prefix}_#{suffix}_key"
+    )
+  end
+
   test "deps with warn_if_outdated: true and hex: option" do
     in_tmp(fn ->
       Mix.Project.push(WarnOutdatedWithHexOption.MixProject)
 
       # This should not crash with KeyError when the package name differs from app name
       assert :ok = Mix.Tasks.Deps.Get.run([])
+    end)
+  end
+
+  test "deps.get does not prompt for auth when only public deps are requested" do
+    in_tmp(fn ->
+      set_home_cwd()
+      store_expired_oauth_token()
+
+      with_project(PublicDepsWithExpiredOAuth.MixProject, fn ->
+        assert :ok = Mix.Tasks.Deps.Get.run([])
+      end)
+
+      refute_received {:mix_shell, :yes?, _}
+    end)
+  end
+
+  test "auth preflight is skipped for public repos" do
+    assert Hex.RemoteConverger.auth_preflight_required?([{"hexpm", "postgrex"}]) == false
+  end
+
+  test "auth preflight is required for organization repos without repo auth" do
+    in_tmp(fn ->
+      set_home_cwd()
+
+      assert Hex.RemoteConverger.auth_preflight_required?([
+               {"hexpm:remote_converger_org", "private_prompt_pkg"}
+             ])
+    end)
+  end
+
+  test "auth preflight is skipped when repo auth is already available" do
+    in_tmp(fn ->
+      set_home_cwd()
+
+      auth = new_repo_auth_user("remote_converger_repo_auth_preflight")
+
+      repos = Hex.State.fetch!(:repos)
+      repos = put_in(repos["hexpm"].auth_key, auth[:key])
+      Hex.State.put(:repos, repos)
+
+      assert Hex.RemoteConverger.auth_preflight_required?([
+               {"hexpm:remote_converger_org", "private_prompt_pkg"}
+             ]) == false
+    end)
+  end
+
+  test "deps.get does not prompt when repo auth is already available" do
+    in_tmp(fn ->
+      set_home_cwd()
+
+      auth = new_repo_auth_user("remote_converger_repo_auth_deps_get")
+
+      repos = Hex.State.fetch!(:repos)
+      repos = put_in(repos["hexpm"].auth_key, auth[:key])
+      Hex.State.put(:repos, repos)
+
+      store_expired_oauth_token()
+
+      with_project(OrganizationDepsWithExpiredOAuth.MixProject, fn ->
+        assert :ok = Mix.Tasks.Deps.Get.run([])
+      end)
+
+      refute_received {:mix_shell, :yes?, _}
     end)
   end
 end
--- test/setup_hexpm.exs
+++ test/setup_hexpm.exs
@@ -89,6 +89,9 @@ Hexpm.new_repo("testorg", auth)
 Hexpm.new_package("testorg", "foo", "0.1.0", [], pkg_meta, auth)
 Hexpm.new_package("testorg", "bar", "0.1.0", [foo: "~> 0.1.0"], pkg_meta, auth)
 
+Hexpm.new_repo("remote_converger_org", auth)
+Hexpm.new_package("remote_converger_org", "private_prompt_pkg", "0.1.0", [], pkg_meta, auth)
+
 Hexpm.new_package("hexpm", "ecto_sql", "3.3.2", [ecto: "~> 3.3.1"], pkg_meta, auth, [
   {"mix.exs", File.read!(Case.fixture_path("ecto_sql_3_3_2/mix.exs"))}
 ])
